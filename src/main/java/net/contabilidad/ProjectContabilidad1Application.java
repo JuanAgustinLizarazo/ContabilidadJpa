6682c4e591e2a385f037bdb163b40a947e200e7f
Juan Agustin Lizarazo Rangel <juan0253@hotmail.com>
2020-04-17 14:03:47 -0500

    Primera parte en el desarrollo de la aplicacion de contabilidad

---
+++ src/main/java/net/contabilidad/ProjectContabilidad1Application.java
@@ -0,0 +1,13 @@
+package net.contabilidad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectContabilidad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectContabilidad1Application.class, args);
+	}
+
+}
